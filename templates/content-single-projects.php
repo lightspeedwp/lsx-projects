a2b158f990ce039bdab54b61c0494fc69b8a371c
Warwick Booth <warwick@lsdev.biz>
2020-09-28 11:16:36 +0200

    Fixing the missing post type notices.

--- templates/content-single-projects.php
+++ templates/content-single-projects.php
@@ -5,197 +5,184 @@
 ?>
 
 <?php
-	$client = get_post_meta( get_the_ID(), 'lsx_project_client', true );
-	$client_logo = get_post_meta( get_the_ID(), 'lsx_project_client_logo', true );
-	$url = get_post_meta( get_the_ID(), 'lsx_project_url', true );
+$client      = get_post_meta( get_the_ID(), 'lsx_project_client', true );
+$client_logo = get_post_meta( get_the_ID(), 'lsx_project_client_logo', true );
+$url         = get_post_meta( get_the_ID(), 'lsx_project_url', true );
 
-	global $lsx_projects;
+global $lsx_projects;
 
-	$button_label = '';
-	$button_cf_id = '';
+$button_label = '';
+$button_cf_id = '';
 
-	if ( ! empty( projects_get_option( 'projects_modal_enable' ) ) ) {
-		if ( ! empty( projects_get_option( 'projects_modal_cta_label' ) ) && ! empty( projects_get_option( 'projects_modal_form_id' ) ) ) {
-			$button_label = projects_get_option( 'projects_modal_cta_label' );
-			$button_cf_id = projects_get_option( 'projects_modal_form_id' );
-		}
+if ( ! empty( projects_get_option( 'projects_modal_enable' ) ) ) {
+	if ( ! empty( projects_get_option( 'projects_modal_cta_label' ) ) && ! empty( projects_get_option( 'projects_modal_form_id' ) ) ) {
+		$button_label = projects_get_option( 'projects_modal_cta_label' );
+		$button_cf_id = projects_get_option( 'projects_modal_form_id' );
+	}
+}
+
+if ( ! empty( $client_logo ) ) {
+	$client_logo = '<img src="' . $client_logo . '">';
+}
+
+$groups = '';
+$terms  = get_the_terms( get_the_ID(), 'project-group' );
+
+if ( $terms && ! is_wp_error( $terms ) ) {
+	$groups = array();
+
+	foreach ( $terms as $term ) {
+		$groups[] = '<a href="' . get_term_link( $term ) . '">' . $term->name . '</a>';
 	}
 
-	if ( ! empty( $client_logo ) ) {
-		$client_logo = '<img src="' . $client_logo . '">';
+	$groups = join( ', ', $groups );
+}
+
+$product_groups = '';
+$product_terms = get_post_meta( get_the_ID(), 'lsx_project__alt_products', false );
+
+if ( $product_terms && ! is_wp_error( $product_terms ) ) {
+	$product_groups = array();
+	foreach ( $product_terms[0] as $product_term ) {
+		$product_groups[] = '<a href="' . $product_term['lsx_project_alt_product_link'] . '">' . $product_term['lsx_project_alt_product_title'] . '</a>';
 	}
 
-	$groups = '';
-	$terms  = get_the_terms( get_the_ID(), 'project-group' );
+	$product_groups = join( ', ', $product_groups );
+}
 
-	if ( $terms && ! is_wp_error( $terms ) ) {
-		$groups = array();
 
-		foreach ( $terms as $term ) {
-			$groups[] = '<a href="' . get_term_link( $term ) . '">' . $term->name . '</a>';
-		}
+$connections = array();
+// Connection Projects.
 
-		$groups = join( ', ', $groups );
+if ( post_type_exists( 'project' ) && $terms && ! is_wp_error( $terms ) ) {
+	$groups_ = array();
+
+	foreach ( $terms as $term ) {
+		$groups_[] = $term->term_id;
 	}
 
-	$product_groups = '';
-	$product_terms = get_post_meta( get_the_ID(), 'lsx_project__alt_products', false );
-
-	if ( $product_terms && ! is_wp_error( $product_terms ) ) {
-		$product_groups = array();
-		foreach ( $product_terms[0] as $product_term ) {
-			$product_groups[] = '<a href="' . $product_term['lsx_project_alt_product_link'] . '">' . $product_term['lsx_project_alt_product_title'] . '</a>';
-		}
-
-		$product_groups = join( ', ', $product_groups );
-	}
-
-
-	// Connections
-
-	$connections = array();
-
-	// Connection Projects
-
-	if ( $terms && ! is_wp_error( $terms ) ) {
-		$groups_ = array();
-
-		foreach ( $terms as $term ) {
-			$groups_[] = $term->term_id;
-		}
-
-		if ( count( $groups_ ) > 0 ) {
-			$connection_project['post_type'] = 'project';
-			$connection_project['title'] = esc_html__( 'Related Projects', 'lsx-projects' );
-			//$connection_project['posts'] = get_post_meta( get_the_ID(), 'project_to_project', false );
-			$connection_project['posts'] = array();
-
-			$args = array(
-				'post_type'              => 'project',
-				'post__not_in'           => array( get_the_ID() ),
-				'no_found_rows'          => true,
-				'ignore_sticky_posts'    => 1,
-				'update_post_meta_cache' => false,
-				'tax_query' => array(
-					array(
-						'taxonomy' => 'project-group',
-						'terms'    => $groups_,
-					),
-				),
-			);
-
-			$projects_ = new \WP_Query( $args );
-
-			if ( $projects_->have_posts() ) {
-				while ( $projects_->have_posts() ) {
-					$projects_->the_post();
-					$connection_project['posts'][] = get_the_ID();
-					wp_reset_postdata();
-				}
-			}
-
-			if ( ! empty( $connection_project['posts'] ) ) {
-				$post_ids = join( ',', $connection_project['posts'] );
-				$connection_project['shortcode'] = '[lsx_projects columns="3" include="' . $post_ids . '"]';
-				$connections[] = $connection_project;
-			}
-		}
-	}
-
-	// Connection Products
-
-	if ( class_exists( 'WooCommerce' ) ) {
-		$connection_product['post_type'] = 'product';
-		$connection_product['title'] = esc_html__( 'Related Products', 'lsx-projects' ) . ' <small>' . esc_html__( 'Products used to build this project', 'lsx-projects' ) . '</small>';
-		$connection_product['posts'] = get_post_meta( get_the_ID(), 'product_to_project', false );
-
-		if ( ! empty( $connection_product['posts'] ) ) {
-			$connection_product['small_list_html'] = '';
-
-			$args = array(
-				'post_type'              => 'product',
-				'post__in'               => $connection_product['posts'][0],
-				'orderby'                => 'post__in',
-				'no_found_rows'          => true,
-				'ignore_sticky_posts'    => 1,
-				'update_post_term_cache' => false,
-				'update_post_meta_cache' => false,
-			);
-
-			$connection_product['posts_obj'] = new \WP_Query( $args );
-
-			if ( $connection_product['posts_obj']->have_posts() ) {
-				$connection_product['small_list_html'] = array();
-
-				while ( $connection_product['posts_obj']->have_posts() ) {
-					$connection_product['posts_obj']->the_post();
-					$connection_product['small_list_html'][] = '<a href="' . get_permalink() . '">' . the_title( '', '', false ) . '</a>';
-					wp_reset_postdata();
-				}
-
-				$connection_product['posts_obj']->rewind_posts();
-				$connection_product['small_list_html'] = join( ', ', $connection_product['small_list_html'] );
-			}
-
-			$connections[] = $connection_product;
-		}
-	}
-
-	// Connection Services
-
-	$connection_service['post_type'] = 'page';
-	// $connection_service['title'] = esc_html__( 'Services', 'lsx-projects' );
-	$connection_service['pages'] = get_post_meta( get_the_ID(), 'page_to_project', false );
-
-	if ( ! empty( $connection_service['pages'] ) ) {
-		$post_ids = join( ',', $connection_service['pages'] );
-		//$connection_service['shortcode'] = '[lsx_services columns="3" include="' . $post_ids . '"]';
-		$connection_service['small_list_html'] = '';
+	if ( count( $groups_ ) > 0 ) {
+		$connection_project['post_type'] = 'project';
+		$connection_project['title']     = esc_html__( 'Related Projects', 'lsx-projects' );
+		$connection_project['posts']     = array();
 
 		$args = array(
-			'post_type'              => 'page',
-			'post__in'               => $connection_service['pages'],
+			'post_type'              => 'project',
+			'post__not_in'           => array( get_the_ID() ),
+			'no_found_rows'          => true,
+			'ignore_sticky_posts'    => 1,
+			'update_post_meta_cache' => false,
+			'tax_query' => array(
+				array(
+					'taxonomy' => 'project-group',
+					'terms'    => $groups_,
+				),
+			),
+		);
+
+		$projects_ = new \WP_Query( $args );
+
+		if ( $projects_->have_posts() ) {
+			while ( $projects_->have_posts() ) {
+				$projects_->the_post();
+				$connection_project['posts'][] = get_the_ID();
+				wp_reset_postdata();
+			}
+		}
+
+		if ( ! empty( $connection_project['posts'] ) ) {
+			$post_ids = join( ',', $connection_project['posts'] );
+			$connection_project['shortcode'] = '[lsx_projects columns="3" include="' . $post_ids . '"]';
+			$connections[] = $connection_project;
+		}
+	}
+}
+
+// Connection Products.
+if ( class_exists( 'WooCommerce' ) ) {
+	$connection_product['post_type'] = 'product';
+	$connection_product['title'] = esc_html__( 'Related Products', 'lsx-projects' ) . ' <small>' . esc_html__( 'Products used to build this project', 'lsx-projects' ) . '</small>';
+	$connection_product['posts'] = get_post_meta( get_the_ID(), 'product_to_project', false );
+
+	if ( ! empty( $connection_product['posts'] ) ) {
+		$connection_product['small_list_html'] = '';
+
+		$args = array(
+			'post_type'              => 'product',
+			'post__in'               => $connection_product['posts'][0],
 			'orderby'                => 'post__in',
 			'no_found_rows'          => true,
 			'ignore_sticky_posts'    => 1,
 			'update_post_term_cache' => false,
 			'update_post_meta_cache' => false,
 		);
+		$connection_product['posts_obj'] = new \WP_Query( $args );
 
-		$services_ = new \WP_Query( $args );
+		if ( $connection_product['posts_obj']->have_posts() ) {
+			$connection_product['small_list_html'] = array();
 
-		if ( $services_->have_posts() ) {
-			$connection_service['small_list_html'] = array();
-
-			while ( $services_->have_posts() ) {
-				$services_->the_post();
-				$connection_service['small_list_html'][] = '<a href="' . get_permalink() . '">' . the_title( '', '', false ) . '</a>';
+			while ( $connection_product['posts_obj']->have_posts() ) {
+				$connection_product['posts_obj']->the_post();
+				$connection_product['small_list_html'][] = '<a href="' . get_permalink() . '">' . the_title( '', '', false ) . '</a>';
 				wp_reset_postdata();
 			}
 
-			$connection_service['small_list_html'] = join( ', ', $connection_service['small_list_html'] );
+			$connection_product['posts_obj']->rewind_posts();
+			$connection_product['small_list_html'] = join( ', ', $connection_product['small_list_html'] );
+		}
+		$connections[] = $connection_product;
+	}
+}
+
+// Connection Services.
+$connection_service['post_type'] = 'page';
+$connection_service['pages']     = get_post_meta( get_the_ID(), 'page_to_project', false );
+
+if ( ! empty( $connection_service['pages'] ) ) {
+	$post_ids                              = join( ',', $connection_service['pages'] );
+	$connection_service['small_list_html'] = '';
+
+	$args = array(
+		'post_type'              => 'page',
+		'post__in'               => $connection_service['pages'],
+		'orderby'                => 'post__in',
+		'no_found_rows'          => true,
+		'ignore_sticky_posts'    => 1,
+		'update_post_term_cache' => false,
+		'update_post_meta_cache' => false,
+	);
+	$services_ = new \WP_Query( $args );
+
+	if ( $services_->have_posts() ) {
+		$connection_service['small_list_html'] = array();
+
+		while ( $services_->have_posts() ) {
+			$services_->the_post();
+			$connection_service['small_list_html'][] = '<a href="' . get_permalink() . '">' . the_title( '', '', false ) . '</a>';
+			wp_reset_postdata();
 		}
 
-		$connections[] = $connection_service;
+		$connection_service['small_list_html'] = join( ', ', $connection_service['small_list_html'] );
 	}
+	$connections[] = $connection_service;
+}
 
-	// Connection Testimonials
-
+// Connection Testimonials.
+if ( post_type_exists( 'testimonial' ) ) {
 	$connection_testimonial['post_type'] = 'testimonial';
-	// $connection_testimonial['title'] = esc_html__( 'Testimonials', 'lsx-projects' );
-	$connection_testimonial['posts'] = get_post_meta( get_the_ID(), 'testimonial_to_project', false );
+	$connection_testimonial['posts']     = get_post_meta( get_the_ID(), 'testimonial_to_project', false );
 
 	if ( ! empty( $connection_testimonial['posts'] ) ) {
 		$post_ids = join( ',', $connection_testimonial['posts'][0] );
 		$connection_testimonial['shortcode'] = '[lsx_testimonials columns="1" include="' . $post_ids . '" orderby="date" order="DESC"]';
 		$connections[] = $connection_testimonial;
 	}
+}
 
-	// Connection Team
-
+// Connection Team.
+if ( post_type_exists( 'team' ) ) {
 	$connection_team['post_type'] = 'team';
-	// $connection_team['title'] = esc_html__( 'Team', 'lsx-projects' );
-	$connection_team['posts'] = get_post_meta( get_the_ID(), 'team_to_project', false );
+	$connection_team['posts']     = get_post_meta( get_the_ID(), 'team_to_project', false );
 
 	if ( ! empty( $connection_team['posts'] ) ) {
 		$post_ids = join( ',', $connection_team['posts'][0] );
@@ -211,7 +198,6 @@
 			'update_post_term_cache' => false,
 			'update_post_meta_cache' => false,
 		);
-
 		$team_ = new \WP_Query( $args );
 
 		if ( $team_->have_posts() ) {
@@ -226,9 +212,9 @@
 
 			$connection_team['small_list_html'] = join( ' ', $connection_team['small_list_html'] );
 		}
-
 		$connections[] = $connection_team;
 	}
+}
 ?>
 
 <?php lsx_entry_before(); ?>
@@ -305,12 +291,12 @@
 	<?php if ( count( $connections ) > 0 ) : ?>
 		<?php foreach ( $connections as $i => $connection ) : ?>
 			<?php
-				// Team is now visible on detail box
-				// Services is now visible on detail box
-				// Testimonials is now visible below the content
-				if ( in_array( $connection['post_type'], array( 'team', 'testimonial', 'service' ) ) ) {
-					continue;
-				}
+			// Team is now visible on detail box.
+			// Services is now visible on detail box.
+			// Testimonials is now visible below the content.
+			if ( in_array( $connection['post_type'], array( 'team', 'testimonial', 'service' ) ) ) {
+				continue;
+			}
 			?>
 			<?php if ( 'page' !== $connection['post_type'] ) { ?>
 				<div class="lsx-projects-section lsx-full-width">
@@ -319,40 +305,39 @@
 							<h3 class="lsx-title"><?php echo wp_kses_post( $connection['title'] ); ?></h3>
 
 							<?php
-								if ( 'product' === $connection['post_type'] ) {
+							if ( 'product' === $connection['post_type'] ) {
 
-									if ( $connection_product['posts_obj']->have_posts() ) {
-										// @codingStandardsIgnoreLine
-										echo apply_filters( 'woocommerce_before_widget_product_list', '<ul class="product_list_widget">' );
+								if ( $connection_product['posts_obj']->have_posts() ) {
+									// @codingStandardsIgnoreLine
+									echo apply_filters( 'woocommerce_before_widget_product_list', '<ul class="product_list_widget">' );
 
-										while ( $connection_product['posts_obj']->have_posts() ) {
-											$connection_product['posts_obj']->the_post();
-											wc_get_template( 'content-widget-product.php', array(
-												'show_rating' => false,
-											) );
+									while ( $connection_product['posts_obj']->have_posts() ) {
+										$connection_product['posts_obj']->the_post();
+										wc_get_template( 'content-widget-product.php', array(
+											'show_rating' => false,
+										) );
 
-											wp_reset_postdata();
-										}
-
-										// @codingStandardsIgnoreLine
-										echo apply_filters( 'woocommerce_after_widget_product_list', '</ul>' );
+										wp_reset_postdata();
 									}
-								} else {
-									echo do_shortcode( $connection['shortcode'] );
+
+									// @codingStandardsIgnoreLine
+									echo apply_filters( 'woocommerce_after_widget_product_list', '</ul>' );
 								}
+							} else {
+								echo do_shortcode( $connection['shortcode'] );
+							}
 							?>
 						</div>
 					</div>
 				</div>
-			<?php
+				<?php
 			}
-		?>
+			?>
 		<?php endforeach; ?>
 	<?php endif; ?>
 
-
 	<?php lsx_entry_bottom(); ?>
-
 </article><!-- #post-## -->
 
-<?php lsx_entry_after();
+<?php
+lsx_entry_after();
